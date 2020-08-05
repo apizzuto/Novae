f524aba39f6b94eb3d90450358b9a96c8dc042e8
Alex Pizzuto <pizzuto@wisc.edu>
2020-08-05 16:37:56 -0500

    full class for individual novae

--- python3/scripts/plotting.py
+++ python3/scripts/plotting.py
@@ -17,34 +17,45 @@ class CatalogPlot():
 class GammaRayNova():
     r'''Holds information about analysis for 
     individual gamma-ray novae'''
-
     def __init__(self, name, delta_t, **kwargs):
         self.delta_t = delta_t
         self.name = name
         self.all_flavor = kwargs.pop('allflavor', True)
-        self.spec_ind = kwargs.pop('index', 2.)
+        self.spec_ind = kwargs.pop('index', [2., 2.5, 3.0])
+        if type(self.spec_ind) is float:
+            self.spec_ind = [self.spec_ind]
         self.min_log_e = kwargs.pop('min_log_e', 0.)
         self.verbose = kwargs.pop('verbose', False)
         self.trials_base = '/data/user/apizzuto/Nova/csky_trials/'
         try:
-            trials_f = glob(self.trials_base + f'nova_*_{self.name}_delta_t_{self.delta_t:.2e}_minLogE_{self.min_log_e:.1f}_gamma_{self.spec_ind:.1f}_allflavor_{self.all_flavor}_trials.pkl')[0]
-            with open(trials_f, 'rb') as f:
-                nova_trials = pickle.load(f)
-            self.sensitivity_trials = nova_trials['sensitivity']
-            self.discovery_trials = nova_trials['discovery']
-            self.fitting_trials = nova_trials['fit']
+            self.sensitivity_trials = {}
+            self.discovery_trials = {}
+            self.fitting_trials = {}
+            for ind in self.spec_ind:
+                trials_path = self.trials_base + f'nova_*_{self.name}_delta_t_{self.delta_t:.2e}_minLogE_{self.min_log_e:.1f}_gamma_{ind:.1f}_allflavor_{self.all_flavor}_trials.pkl'
+                trials_f = glob(trials_path)[0]
+                with open(trials_f, 'rb') as f:
+                    nova_trials = pickle.load(f)
+                self.sensitivity_trials[ind] = nova_trials['sensitivity']
+                self.discovery_trials[ind] = nova_trials['discovery']
+                self.fitting_trials[ind] = nova_trials['fit']
             self.background = nova_trials['bg']
             self.ra = nova_trials['source_info']['ra']
             self.dec = nova_trials['source_info']['dec']
         except Exception as e:
             if self.verbose:
                 print(f"Could not find trials for Nova {self.name} with analysis details:")
-                print(f"\t All flavor: {self.all_flavor}\t Gamma: {self.spec_ind}\t Min E: log10({self.min_log_e})")
+                print(f"\t All flavor: {self.all_flavor}\t Gamma: {self.spec_ind}\t Min log10(E): {self.min_log_e}")
                 print(f"\t Duration: {self.delta_t:.2e} s")
         self.fontsize = kwargs.pop('fontsize', 16)
         self.units_ref_str = 'TeV cm^-2 @ 1 TeV'
+        self.gamma_colors = {2.0: 'C0', 2.5: 'C1', 3.0: 'C2'}
+        self.central_90 = {2.0: (23.26, 4430.12), 2.5: (8.69, 1594.00), 
+                           3.0: (4.76, 419.32)}
                 
     def background_ts_plot(self, ax=None, label_axes=True, **kwargs):
+        r'''Make plot showing chi2 distribution and observed TS
+        for background trials'''
         if ax is None:
             fig, ax = plt.subplots()
         bg = self.background
@@ -60,6 +71,107 @@ class GammaRayNova():
         if label_axes:
             ax.set_xlabel('TS', fontsize=self.fontsize)
             ax.set_ylabel('Number of Trials', fontsize=self.fontsize)
+            
+    def ns_fit_bias_plot(self, ax=None, label_axes=True, **kwargs):
+        r'''Compare fit and injected number of events'''
+        gamma = kwargs.pop('gamma', self.spec_ind[0])
+        if ax is None:
+            fig, ax = plt.subplots()
+        n_sigs = np.unique(self.fitting_trials[gamma].ntrue)
+        dns = np.mean(np.diff(n_sigs))
+        ns_bins = np.r_[n_sigs - 0.5*dns, n_sigs[-1] + 0.5*dns]
+        h = hl.hist((self.fitting_trials[gamma].ntrue, 
+                     self.fitting_trials[gamma].ns), 
+                    bins=(ns_bins, 100))
+        hl.plot1d(ax, h.contain_project(1), 
+                  errorbands=True, drawstyle='default', color=self.gamma_colors[gamma])
+        lim = [0., max(n_sigs)+1]
+        ax.set_xlim(ax.set_ylim(lim))
+        expect_kw = dict(color=self.gamma_colors[gamma], 
+                         ls='--', lw=1, zorder=-10)
+        ax.plot(lim, lim, **expect_kw)
+        if label_axes:
+            ax.set_xlabel(r'$n_\mathrm{inj}$', fontsize=self.fontsize)
+            ax.set_ylabel(r'$\hat{n_s}$', fontsize=self.fontsize)
+
+    def gamma_fit_bias_plot(self, ax=None, label_axes=True, **kwargs):
+        r'''Compare fit spectral indices as a function of signal strength'''
+        if ax is None:
+            fig, ax = plt.subplots()
+        n_sigs = np.unique(self.fitting_trials[self.spec_ind[0]].ntrue)
+        dns = np.mean(np.diff(n_sigs))
+        ns_bins = np.r_[n_sigs - 0.5*dns, n_sigs[-1] + 0.5*dns]
+        for gamma in self.spec_ind:
+            h = hl.hist((self.fitting_trials[gamma].ntrue, 
+                         self.fitting_trials[gamma].gamma), 
+                        bins=(ns_bins, 100))
+            hl.plot1d(ax, h.contain_project(1), errorbands=True, 
+                      drawstyle='default', color=self.gamma_colors[gamma])
+            expect_kw = dict(color=self.gamma_colors[gamma], 
+                         ls='--', lw=1, zorder=-10)
+            ax.axhline(gamma, **expect_kw)
+        if label_axes:
+            ax.set_xlabel(r'$n_\mathrm{inj}$', fontsize=self.fontsize)
+            ax.set_ylabel(r'$\hat{\gamma}$', fontsize=self.fontsize)
+
+    def compare_sens_to_photons(self, ax=None, label_axes=True, **kwargs):
+        r'''Plot time integrated sensitivities and compare them to 
+        the best-fit gamma-ray spectra'''
+        gamma_df = pd.read_csv('/home/apizzuto/Nova/gamma_ray_novae.csv')
+        try:
+            gamma_info = gamma_df.loc[gamma_df['Name'] == self.name]
+            gamma = float(gamma_info['Index'])
+            integrated_norm = float(gamma_info['Flux'])*1e-7
+            cutoff = float(gamma_info['Cutoff'])
+        except:
+            print(f"Could not find Nova {self.name} in dataframe")
+            return
+        if ax is None:
+            fig, ax = plt.subplots()
+        ens = np.logspace(-1., 3., 500)
+        photon_norm = self.calc_flux_norm(gamma, cutoff, integrated_norm)
+        if np.isinf(cutoff):
+            photon_flux = photon_norm * np.power(ens, -gamma)
+        else:
+            photon_flux = photon_norm * np.power(ens, -gamma) * \
+                            np.exp(-ens / cutoff)
+        ax.plot(ens, photon_flux* ens**2. * self.delta_t, 
+                color=sns.xkcd_rgb['battleship grey'], alpha=0.7)
+        
+        ######## CONVERT TO NEUTRINO EXPECTATION?
+        
+        handles = []
+        for spec in self.spec_ind:
+            nu_sens_spec = np.power(ens, -spec)*self.sensitivity_trials[spec]['E2dNdE']*1e3*(1e-3)**(2.-spec)
+            en_msk = (ens > self.central_90[spec][0]) & (ens < self.central_90[spec][1])
+            ax.plot(ens[en_msk], nu_sens_spec[en_msk]*ens[en_msk]**2,
+                   color=self.gamma_colors[spec], lw = 1.0)
+            handles.append(Line2D([0], [0], color=self.gamma_colors[spec], 
+                                  lw=1.5, label=r"$\gamma = $" + f" {spec:.1f}"))
+        ax.loglog()
+        ax.set_ylim(1e-3, 3e4)
+        ax.set_xlim(1e-1, 1e3)
+        if label_axes:
+            ax.set_xlabel(r"$E$ (GeV)", fontsize=self.fontsize)
+            ax.set_ylabel(r"$E^2 \frac{dN}{dE}$ (GeV cm$^{-2}$)", fontsize = self.fontsize)
+            
+        ax.legend(handles=handles, frameon=False)
+        
+    def calc_flux_norm(self, gamma, cutoff, norm):
+        r'''Fermi fluxes are reported in terms of integral fluxes
+        above 100 MeV. Convert them here to the same units used here
+        Parameters:
+        -----------
+            -norm: float, integrated flux above 100 MeV in units of photons per cm^-2 s^-1
+        Returns:
+        --------
+            Flux normalization at 1 GeV in units of GeV^-1 cm^-2 s^1
+        '''
+        if np.isinf(cutoff):
+            denom = 0.1*np.exp(2.30259*gamma) / (-1. + gamma)
+        else:
+            denom = cutoff ** (1.-gamma)*float(mpmath.gammainc(1.-gamma, 0.1/cutoff))
+        return norm / denom
 
 class GRECOPlots():
     r'''Helper class to make plots that describe the 
