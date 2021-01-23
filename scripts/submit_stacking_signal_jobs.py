249d143fa8fbfe3cac743992e9d096c656a2b99f
Alex Pizzuto <pizzuto@wisc.edu>
2021-01-23 13:01:43 -0600

    resubmit large windows with fewer trials

--- scripts/submit_stacking_signal_jobs.py
+++ scripts/submit_stacking_signal_jobs.py
@@ -47,25 +47,27 @@ high_mem_signal = pycondor.Job(
 	)
 
 for allflavor_str in [' --allflavor', '']:
-	for deltaT in np.append(np.logspace(-3., 1., 9)[:]*86400., np.array([86400.*5.])):
-		if deltaT > 86400.:
-			ntrials_sig = 100
-		else:
-			ntrials_sig = 250
-		for gamma in [2.0, 2.5, 3.0]:
-			for cut in [0.0, 0.5, 1.0]:
-				if deltaT > 86400:
-					high_mem_signal.add_arg(
-                        f'--deltaT={deltaT} --index={gamma} --minLogE={cut}' \
+	#for deltaT in np.append(np.logspace(-3., 1., 9)[:]*86400., np.array([86400.*5.])):
+    for deltaT in [86400.*10.]:
+        if deltaT > 86400.*5.:
+            ntrials_sig = 50
+        elif deltaT > 86400.:
+            ntrials_sig = 100
+        else:
+            ntrials_sig = 250
+        for gamma in [2.0, 2.5, 3.0]:
+            for cut in [0.0, 0.5, 1.0]:
+                if deltaT > 86400:
+                    high_mem_signal.add_arg(
+                    f'--deltaT={deltaT} --index={gamma} --minLogE={cut}' \
                         + f' --ntrials_sig={ntrials_sig}' \
                         + f'{allflavor_str}'
                         )
-				else:
-					low_mem_signal.add_arg(
+                else:
+                    low_mem_signal.add_arg(
                         f'--deltaT={deltaT} --index={gamma} --minLogE={cut}' \
                         + f' --ntrials_sig={ntrials_sig}' \
                         + f'{allflavor_str}'
                         ) 
-
 
 dagman.build_submit()
