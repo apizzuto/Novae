327ce62fb0b809c525b3de6462ecd42474dfd6de
Alex Pizzuto <pizzuto@wisc.edu>
2021-03-11 16:13:32 -0600

    Fixed everything to do with the catalog

--- scripts/create_master_nova_dataframe.py
+++ scripts/create_master_nova_dataframe.py
@@ -96,58 +96,129 @@ novae = novae.drop(columns=["Discoverer", "Refs", "T3", "Class"], inplace = Fals
 novae['RA'], novae['Dec'] = format_coords(novae['RA'], novae['Dec'])
 novae['Variable'] = input_name(novae['Variable'], novae['Obscure xid'])
 
+###############################################################################
+#######   Load novae from x-ray/gamma analysis (used for gamma times)      ####
+###############################################################################
+xray_tab = Table.read('/home/apizzuto/Nova/source_list/gamma_nova_from_xray_paper.tex').to_pandas()
+
+tstart = []
+for t in xray_tab['Time$_{\\rm \\gamma-ray\\ start}$']:
+    if 'table' in t:
+        t = t.split('\\')[0]
+    tmp = Time(t, format='mjd')
+    tstart.append(tmp)
+    
+tstop = []
+for t in xray_tab['Time$_{\\rm \\gamma-ray\\ end}$']:
+    if 'table' in t:
+        t = t.split('\\')[0]
+        t = t.split('--')[0]
+    tmp = Time(t, format='mjd')
+    tstop.append(tmp)
+
+xray_tab['Time$_{\\rm \\gamma-ray\\ start}$'] = tstart
+xray_tab['Time$_{\\rm \\gamma-ray\\ end}$'] = tstop
+
+###############################################################################
+#######             Load novae from optical peak time stuff              ######
+###############################################################################
+optical_info = pd.read_csv('/home/apizzuto/Nova/source_list/nova_optical_peaks.csv')
+optical_info['OpticalPeak'] = [Time(t, format='iso') for t in optical_info['OpticalPeak']]
 
 ###############################################################################
 #######              Create one master dataframe for it all      ##############
 ###############################################################################
 master_dict = dict(Name = [], Date = [], Peak = [], RA = [], Dec = [], gamma = [],
                   gamma_start = [], gamma_stop = [], gamma_norm = [],
-                  gamma_ind = [], gamma_cutoff = [])
+                  gamma_ind = [], gamma_cutoff = [],
+                  refs = [], )
 all_names = set(novae['Variable']) | set(df['Name']) | set(gamma_df['Name'])
 
 for name in all_names:
+    print(f"Beginning {name}")
+    if name in ["V1404 Cen", "V5854 Sgr", "V1657 Sco", "V3663 Oph", 
+        "FM Cir", "V3731 Oph", "V3730 Oph", "V2891 Cyg", "V1709 Sco"]:
+        print(f"Skipping {name}")
+        continue
+    master_dict['refs'].append('')
     if name in gamma_df['Name'].unique():
+        print(f"\t - is gamma nova")
         df_ind = gamma_df[gamma_df['Name'] == name].index.values[0]
         for mkey, key in [('Name', 'Name'), ('RA', 'RA'), ('Dec', 'Dec'), 
-                         ('gamma_start', 'Start Time'), ('gamma_stop', 'Stop Time'),
+                         # ('gamma_start', 'Start Time'), ('gamma_stop', 'Stop Time'),
                          ('gamma_norm', 'Flux'), ('gamma_ind', 'Index'),
                          ('gamma_cutoff', 'Cutoff')]:
             master_dict[mkey].append(gamma_df[key][df_ind])
+        if name in xray_tab['Nova'].unique():
+            print(f"\t - gamma nova {name} included in x-ray analysis,")
+            print("\t\t using gamma time from that paper")
+            xind = xray_tab[xray_tab['Nova'] == name].index.values[0]
+            for mkey, x_key, key in [('gamma_start', 'Time$_{\\rm \\gamma-ray\\ start}$', 'Start Time'), 
+                ('gamma_stop', 'Time$_{\\rm \\gamma-ray\\ end}$', 'Stop Time')]:
+                master_dict[mkey].append(xray_tab[x_key][xind])
+            master_dict['refs'][-1] = master_dict['refs'][-1] + 'xray:' + xray_tab['Reference'][xind]
+        else:
+            print(f"\t - Gamma nova {name} not included in x-ray analysis")
+            print("\t\t using gamma time from our own list")
+            for mkey, x_key, key in [('gamma_start', 'Time$_{\\rm \\gamma-ray\\ start}$', 'Start Time'), 
+                ('gamma_stop', 'Time$_{\\rm \\gamma-ray\\ end}$', 'Stop Time')]:
+                master_dict[mkey].append(gamma_df[key][df_ind])
             
         if name in df['Name'].unique():
             df_ind = df[df['Name'] == name].index.values[0]
-            print("Gamma nova found in Anna's paper, using peak from that", name)
+            print("\t - Gamma nova found in Anna's paper, using peak from that", name)
             for mkey, key in [('Date', 'Peak Time'), ('Peak', 'Peak')]:
                 master_dict[mkey].append(df[key][df_ind])
-        elif name in novae['Variable'].unique():
-            print("Gamma nova found in galnovae file, using peak from that", name)
-            df_ind = novae[novae['Variable'] == name].index.values[0]
-            for mkey, key in [('Date', 'Date'), ('Peak', 'Max Mag.')]:
-                master_dict[mkey].append(novae[key][df_ind])
-            
-        elif name == 'V3890 Sgr':
-            print('Using data from ATels for V3890 Sgr')
-            master_dict['Date'].append(Time("2019-08-28 00:00:00") + TimeDelta(float(f"0.188")*86400., format='sec'))
-            master_dict['Peak'].append(6.7)
+            master_dict['refs'][-1] = master_dict['refs'][-1] + 'Anna'  
+        elif name in optical_info['Name'].unique():
+            opt_ind = optical_info[optical_info['Name'] == name].index.values[0]
+            print(f"\t - Gamma nova {name} found in our list of optical times")
+            print("\t\t using time from that list")
+            master_dict['Date'].append(optical_info['OpticalPeak'][opt_ind])
+            master_dict['refs'][-1] = master_dict['refs'][-1] + 'optical:' + optical_info['Ref'][opt_ind]
+            if name in novae['Variable'].unique():
+                print("\t - Taking peak mag from galnovae file for", name)
+                df_ind = novae[novae['Variable'] == name].index.values[0]
+                for mkey, key in [('Peak', 'Max Mag.')]:
+                    master_dict[mkey].append(novae[key][df_ind]) 
+            else:
+                if name == 'V3890 Sgr':
+                    print('\t - Using data from ATels for V3890 Sgr')
+                    master_dict['Peak'].append(6.7)
+                    master_dict['refs'][-1] = master_dict['refs'][-1] + 'optical:Atel13047'
+                else:
+                    print(f"\t - NO MAX MAGNITUDE FOR NOVA {name}")
         else:
-            print("COULD NOT FIND GAMMA NOVA IN EITHER, NO TIME AVAILABLE?", name)
+            print("\t - COULD NOT FIND GAMMA NOVA IN EITHER, NO TIME AVAILABLE?", name)
             for mkey, key in [('Date', 'Date'), ('Peak', 'Max Mag.')]:
                 master_dict[mkey].append(np.nan)
         master_dict['gamma'].append(True)
         
     elif name in df['Name'].unique():
+        print(f"\t - Found in Anna's paper, all info from there")
         df_ind = df[df['Name'] == name].index.values[0]
         for mkey, key in [('Name', 'Name'), ('Date', 'Peak Time'), ('Peak', 'Peak'),
                          ('RA', 'ra'), ('Dec', 'dec'), ('gamma', 'gamma')]:
             master_dict[mkey].append(df[key][df_ind])
         for mkey in ['gamma_start', 'gamma_stop', 'gamma_norm', 'gamma_ind', 'gamma_cutoff']:
             master_dict[mkey].append(np.nan)
+        master_dict['refs'][-1] = master_dict['refs'][-1] + 'Anna'
     
     elif name in novae['Variable'].unique():
+        print(f"\t - Found in Galnovae file, getting info from there")
         df_ind = novae[novae['Variable'] == name].index.values[0]
-        for mkey, key in [('Name', 'Variable'), ('Date', 'Date'), ('Peak', 'Max Mag.'),
+        for mkey, key in [('Name', 'Variable'), ('Peak', 'Max Mag.'),
                          ('RA', 'RA'), ('Dec', 'Dec')]:
             master_dict[mkey].append(novae[key][df_ind])
+        if name in optical_info['Name'].unique():
+            print(f"\t - Optical peak time from local file")
+            opt_ind = optical_info[optical_info['Name'] == name].index.values[0]
+            for mkey, opt_key in [('Date', 'OpticalPeak')]:
+                master_dict[mkey].append(optical_info[opt_key][opt_ind])
+            master_dict['refs'][-1] = master_dict['refs'][-1] + 'optical: ' + optical_info['Ref'][opt_ind]
+        else:
+            print(f"\t - No info on optical peak for {name}")
+            master_dict['Date'].append(np.nan)
         master_dict['gamma'].append(False)
         for mkey in ['gamma_start', 'gamma_stop', 'gamma_norm', 'gamma_ind', 'gamma_cutoff']:
             master_dict[mkey].append(np.nan)
@@ -164,12 +235,20 @@ min_greco_mjd = np.min(np.load('/data/user/apizzuto/Nova/GRECO_Skylab_Dataset/v2
 min_greco_time = Time(min_greco_mjd, format='mjd')
 
 max_greco_mjd = np.max(np.load('/data/user/apizzuto/Nova/GRECO_Skylab_Dataset/v2.4/IC86_2018.data.npy')['time'])
+max_greco_mjd = 59000. # End of May 2020 lines up with end of 2019 data
 max_greco_time = Time(max_greco_mjd, format='mjd')
 
 master_df = master_df[master_df['Date'] > min_greco_time]
 master_df = master_df[master_df['Date'] < max_greco_time]
 
 master_df = master_df.reset_index()
+
+# pd.set_option('display.max_rows', 100)
+# pd.options.display.max_colwidth = 100
+# print(master_df)
+# import sys
+# sys.exit()
+
 master_df.to_pickle('/home/apizzuto/Nova/master_nova_dataframe.pkl')
 
 ###############################################################################
@@ -182,7 +261,7 @@ gplane_icrs = gplane.icrs
 gcent = SkyCoord(frame='galactic', b = [0.0]*u.degree, l = [0.0]*u.degree)
 gcent_icrs = gcent.icrs
 cols = [sns.xkcd_rgb['orange pink'] if k is True else sns.xkcd_rgb['light navy blue'] for k in master_df['gamma']]
-s = [14 if k is True else 10 for k in df['gamma']]
+s = [14 if k is True else 10 for k in master_df['gamma']]
 
 legend_els = [ Line2D([0], [0], marker='o', ls = '', color=sns.xkcd_rgb['orange pink'], label=r'$\gamma$ detected'),
               Line2D([0], [0], marker='o', ls = '', color=sns.xkcd_rgb['light navy blue'], label='Optical only')]
@@ -194,8 +273,8 @@ ax.grid(True, alpha = 0.35, zorder=1, ls = '--')
 equatorial = SkyCoord(ra=master_df['RA']*u.deg, dec=master_df['Dec']*u.deg)
 ax.scatter(-1*equatorial.ra.wrap_at('360d').radian + np.pi, equatorial.dec.radian, 
            zorder=20, s = s, c = cols)
-ax.scatter(-1*gamma_coords.ra.wrap_at('360d').radian + np.pi, gamma_coords.dec.radian, 
-           zorder=20, s = s, c = sns.xkcd_rgb['orange pink'])
+# ax.scatter(-1*gamma_coords.ra.wrap_at('360d').radian + np.pi, gamma_coords.dec.radian, 
+#            zorder=20, s = s, c = sns.xkcd_rgb['orange pink'])
 
 ax.scatter(-1.*gplane_icrs.ra.wrap_at('360d').radian + np.pi, gplane_icrs.dec.radian, 
            zorder=10, c = 'k', s = 0.5)
