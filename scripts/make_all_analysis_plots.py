cf00a750756be598a1362da1f527c50aa10d2f05
Alex Pizzuto <pizzuto@wisc.edu>
2021-03-22 15:08:15 -0500

    add synthesis plots and fix others

--- scripts/make_all_analysis_plots.py
+++ scripts/make_all_analysis_plots.py
@@ -1,5 +1,5 @@
 import argparse
-from plotting import StackingPlots, GammaCatalog, GammaRayNova, GRECOPlots
+from plotting import StackingPlots, GammaCatalog, GammaRayNova, GRECOPlots, SynthesisPlots
 import matplotlib as mpl
 mpl.use('agg')
 import matplotlib.pyplot as plt
@@ -29,28 +29,33 @@ def make_gamma_correlation_plots(cat):
     print("\t - Finished plots with fixed time windows")
 
 def make_stacking_plots(stack):
-    print("\n\t Beginning stackcing plots")
+    print("\n\t Beginning stacking plots")
     stack.get_all_sens()
     stack.sensitivity_plot()
     stack.background_distribution()
     stack.background_vs_time()
     #stack.likelihood_scan(truth=True)
     stack.fitting_plot(gamma=[2.0, 2.5, 3.])
-    for gam in [2.0, 2.5, 3.0]:
-        for disc in [True, False]:
-            stack.sensitivity_efficiency_curve(
-                gamma=gam, discovery=disc
-                )
+    # for gam in [2.0, 2.5, 3.0]:
+    #     for disc in [True, False]:
+    #         stack.sensitivity_efficiency_curve(
+    #             gamma=gam, discovery=disc
+    #             )
     print("\t Finished stacking plots")
     
-def make_GRECO_plots():
-    gplots = GRECOPlots(min_log_e=args.minLogE, allflavor=args.allflavor, save=True)
+def make_GRECO_plots(gplots):
     gplots.declination_pdf()
     gplots.energy_pdf()
     for true_en in [True, False]:
         for true_err in [True, False]:
             gplots.angular_error_plot(true_energy=true_en, true_error=true_err)
     gplots.errors_vs_declination()
+
+def make_synthesis_plots(syn_plots):
+    syn_plots.gamma_lightcurve_with_greco_rate()
+    syn_plots.all_sky_scatter_plot()
+    syn_plots.mollview_with_sensitivity()
+
     
 
 if __name__ == '__main__':
@@ -70,15 +75,22 @@ if __name__ == '__main__':
     make_gamma_correlation_plots(cat)
 
     print("Making figures for the stacked analysis with parameters:")
-    print(f"\t Min log10(E): {args.minLogE:.1f}, \t All flavor: {args.allflavor}")
+    print(f"\t Min log10(E): None (not tested for stacking), \t All flavor: {args.allflavor}")
     print(f"\t Stacking time window: {args.stacking_time:.2e} s")
     print(f"\t Figures will be saved to {args.output_path}")
     stack_plots = StackingPlots(
-        args.stacking_time, min_log_e=args.minLogE, allflavor=args.allflavor,
+        args.stacking_time, min_log_e=None, allflavor=args.allflavor,
         save=True, output=args.output_path)
     make_stacking_plots(stack_plots)
 
     print("Making figures for the GRECO dataset with parameters:")
     print(f"\t Min log10(E): {args.minLogE:.1f}, \t All flavor: {args.allflavor}")
     print(f"\t Figures will be saved to {args.output_path}")
-    make_GRECO_plots()
+    gplots = GRECOPlots(min_log_e=args.minLogE, 
+        allflavor=args.allflavor, save=True)
+    make_GRECO_plots(gplots)
+
+    print("Making figures that synthesize top level nova info and GRECO info:")
+    print(f"\t Figures will be saved to {args.output_path}")
+    syn_plots = SynthesisPlots(save=True, savedir=args.output_path)
+    make_synthesis_plots(syn_plots)
