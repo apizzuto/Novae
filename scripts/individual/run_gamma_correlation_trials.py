03a59d8ef786c24dc3d476323d775c271b13a3f6
Alex Pizzuto <pizzuto@wisc.edu>
2021-08-30 12:17:33 -0500

    linted and new version 2.10 dataset

--- scripts/individual/run_gamma_correlation_trials.py
+++ scripts/individual/run_gamma_correlation_trials.py
@@ -1,5 +1,4 @@
-import pycondor, argparse, sys, os.path
-from glob import glob
+import pycondor
 import numpy as np
 import pandas as pd
 
@@ -8,7 +7,8 @@ output = '/scratch/apizzuto/novae/condor/output'
 log = '/scratch/apizzuto/novae/condor/log'
 submit = '/scratch/apizzuto/novae/condor/submit'
 
-job = pycondor.Job('sensitivity_stacking_novae_greco',
+job = pycondor.Job(
+    'sensitivity_stacking_novae_greco',
     '/home/apizzuto/Nova/scripts/individual/gamma_nova_sensitivity.py',
     error=error,
     output=output,
@@ -16,28 +16,34 @@ job = pycondor.Job('sensitivity_stacking_novae_greco',
     submit=submit,
     getenv=True,
     universe='vanilla',
-    verbose=2, 
+    verbose=2,
     request_memory=6000,
-    extra_lines= ['should_transfer_files = YES', 'when_to_transfer_output = ON_EXIT', 'Requirements =  (Machine != "node128.icecube.wisc.edu")']
+    extra_lines=[
+        'should_transfer_files = YES',
+        'when_to_transfer_output = ON_EXIT',
+        'Requirements =  (Machine != "node128.icecube.wisc.edu")']
     )
 
 master_df = pd.read_pickle('/home/apizzuto/Nova/master_nova_dataframe.pkl')
-gamma_df = master_df[master_df['gamma']==True]
+gamma_df = master_df[master_df['gamma']]
 gamma_df = gamma_df.reset_index()
 
 for nova_num in range(len(gamma_df)):
-    for deltaT in np.append(np.logspace(-1.5, 1., 6)[:]*86400., np.array([86400.*5.])):
+    for deltaT in np.append(np.logspace(-1.5, 1., 6)[:]*86400.,
+                            np.array([86400.*5.])):
         for gamma in [2.0, 2.5, 3.0]:
-            for cut in [0.0, 0.5, 1.0]:
-                flav = ' --allflavor'
-                job.add_arg('--nova_num={} --deltaT={} --index={} --minLogE={}{}'.format(nova_num, deltaT, gamma, cut, flav))
+            job.add_arg(
+                '--nova_num={} --deltaT={} --index={} --minLogE={}{}'.format(
+                    nova_num, deltaT, gamma))
 
 for nova_num in range(len(gamma_df)):
     for gamma in [2.0, 2.5, 3.0]:
-        for cut in [0.0, 0.5, 1.0]:
-            flav = ' --allflavor'
-            job.add_arg('--nova_num={} --index={} --full_gamma_time --minLogE={}{}'.format(nova_num, gamma, cut, flav))
+        job.add_arg(
+            f'--nova_num={nova_num} --index={gamma} '
+            + '--full_gamma_time')
 
-dagman = pycondor.Dagman('Skylab_Novae_sensitivity_trials', submit=submit, verbose=2)
+dagman = pycondor.Dagman(
+    'Skylab_Novae_sensitivity_trials',
+    submit=submit, verbose=2)
 dagman.add_job(job)
 dagman.build_submit()
--- scripts/individual/sensitivity_vs_declination.py
+++ scripts/individual/sensitivity_vs_declination.py
@@ -1,76 +1,45 @@
 #!/usr/bin/env python
 
 import numpy as np
-from scipy import stats
-import pandas as pd
-from astropy import units as u
-from astropy.coordinates import SkyCoord
-from astropy.time import Time
-from datetime import datetime
 import argparse
-import matplotlib as mpl
-mpl.use('Agg')
-import histlite as hl
-import csky as cy
 import pickle
 from glob import glob
-import sys
 
-parser = argparse.ArgumentParser(description='Sensitivity vs declination for a fixed time window')
-parser.add_argument('--deltaT', type=float, default = 1000.,
-                    help='Time window in seconds')
+import csky as cy
+
+parser = argparse.ArgumentParser(
+    description='Sensitivity vs declination for a fixed time window')
+parser.add_argument(
+    '--deltaT', type=float, default=1000.,
+    help='Time window in seconds')
 parser.add_argument('--index', type=float, default=1, help='Spectral Index')
-parser.add_argument('--minLogE', type=float, default=None, help='Cut on the minimum reco energy')
-parser.add_argument('--ntrials_bg', type=int, default=10000, help="Number of background trials")
-parser.add_argument('--ntrials_sig', type=int, default=500, help='Number of trials per signal strength')
-parser.add_argument('--allflavor', action='store_true', default=False, help="All neutrino flavors in MC")
+parser.add_argument(
+    '--ntrials_bg', type=int, default=10000,
+    help="Number of background trials")
+parser.add_argument(
+    '--ntrials_sig', type=int, default=500,
+    help='Number of trials per signal strength')
 args = parser.parse_args()
 
 delta_t = args.deltaT
 delta_t_days = delta_t / 86400.
 
-greco_base = '/data/user/apizzuto/Nova/GRECO_Skylab_Dataset/v2.5/'
-
-data_fs = sorted(glob(greco_base + 'IC86_20*data_with_angErr.npy'))
-exp = [np.load(data) for data in data_fs]
-exp = np.hstack(exp)
-if args.allflavor:
-    mcfiles = glob(greco_base + 'IC86_2012.nu*_with_angErr.npy')
-    mc = np.load(mcfiles[0])
-    for flav in mcfiles[1:]:
-        mc = np.concatenate((mc, np.load(flav)))
-else:
-    mcfile = glob(greco_base + 'IC86_2012.numu_merged_with_angErr.npy')[0]
-    mc = np.load(mcfile)
-grls = sorted(glob(greco_base + 'GRL/IC86_20*data.npy'))
-grl = [np.load(g) for g in grls]
-grl = np.hstack(grl)
-
-if args.minLogE is not None:
-    exp_msk = exp['logE'] > args.minLogE
-    exp = exp[exp_msk]
-    mc_msk = mc['logE'] > args.minLogE
-    mc = mc[mc_msk]
-    low_en_bin = args.minLogE
-else:
-    low_en_bin = 0.
-
-greco = cy.selections.CustomDataSpecs.CustomDataSpec(exp, mc, np.sum(grl['livetime']), 
-                                                     np.linspace(-1., 1., 31),
-                                                     np.linspace(low_en_bin, 4., 31), 
-                                                     grl=grl, key='GRECOv2.5', cascades=True)
-
 ana_dir = cy.utils.ensure_dir('/data/user/apizzuto/csky_cache/greco_ana')
-greco_ana = cy.get_analysis(cy.selections.repo, greco, dir=ana_dir)
+greco_ana = cy.get_analysis(
+    cy.selections.repo,
+    'version-002-p10',
+    cy.selections.GRECOOnlineDataSpecs.GRECO_IC86_2012_2019,
+    dir=ana_dir)
 
-conf = {'ana': greco_ana,
-       'extended': True,
-       'space': "ps",
-        'time': "transient",
-        'sig': 'transient',
-       }
+conf = {
+    'ana': greco_ana,
+    'extended': True,
+    'space': "ps",
+    'time': "transient",
+    'sig': 'transient'}
 
-result_dict = {'dec': [], 
+result_dict = {
+    'dec': [],
     'sens': [],
     'sens_nsig': []}
 
@@ -78,41 +47,38 @@ ra = 0.
 mjd_start = 57200.
 sin_decs = np.linspace(-0.95, 0.95, 20)
 for dec in np.arcsin(sin_decs):
-    src = cy.utils.Sources(ra=ra, 
-                        dec=dec, 
-                        mjd=mjd_start, 
-                        sigma_t=0., 
-                        t_100=delta_t_days)
+    src = cy.utils.Sources(
+        ra=ra, dec=dec, mjd=mjd_start,
+        sigma_t=0., t_100=delta_t_days)
     cy.CONF['src'] = src
     cy.CONF['mp_cpus'] = 5
 
     tr = cy.get_trial_runner(conf, ana=greco_ana, src=src)
     bg = cy.dists.Chi2TSD(tr.get_many_fits(args.ntrials_bg))
 
-    tr = cy.get_trial_runner(conf, ana=greco_ana, src=src, 
+    tr = cy.get_trial_runner(
+        conf, ana=greco_ana, src=src,
         inj_conf={'flux': cy.hyp.PowerLawFlux(args.index)})
 
     ########################################################################
-    ################ SENSITIVITY CALCULATION ###############################
+    # SENSITIVITY CALCULATION
     ########################################################################
     nsig_step = 1. if delta_t < 864000. else 20.
 
-    sensitivity = tr.find_n_sig(bg.median(), 0.9, 
-                        batch_size=args.ntrials_sig,
-                        n_sig_step=nsig_step,
-                        max_batch_size=0, 
-                        logging=True, 
-                        n_bootstrap=1)
+    sensitivity = tr.find_n_sig(
+        bg.median(), 0.9, batch_size=args.ntrials_sig,
+        n_sig_step=nsig_step,
+        max_batch_size=0, logging=True,
+        n_bootstrap=1)
     sensitivity['E2dNdE'] = tr.to_E2dNdE(sensitivity, E0=1., unit=1e3)
 
     result_dict['dec'].append(dec)
     result_dict['sens'].append(sensitivity['E2dNdE'])
     result_dict['sens_nsig'].append(sensitivity['n_sig'])
 
-
-add_str = 'minLogE_{:.1f}_'.format(args.minLogE) if args.minLogE is not None else ''
 delta_t_str = f"{delta_t:.2e}"
-output_loc = f"/data/user/apizzuto/Nova/csky_trials/sens_vs_dec/sens_vs_dec_delta_t_{delta_t_str}_{add_str}gamma_{args.index}_allflavor_{args.allflavor}_trials.pkl"
+output_loc = "/data/user/apizzuto/Nova/csky_trials/sens_vs_dec/" \
+    + f"sens_vs_dec_delta_t_{delta_t_str}_gamma_{args.index}_trials.pkl"
 
 with open(output_loc, 'wb') as f:
     pickle.dump(result_dict, f)
