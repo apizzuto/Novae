5d654d0c80bb3c654cc9b450a72ac1826b6b78c7
Alex Pizzuto <pizzuto@wisc.edu>
2021-08-30 13:17:46 -0500

    submit scripts updated to pep8

--- scripts/stacking/submit_all_stacking_jobs.py
+++ scripts/stacking/submit_all_stacking_jobs.py
@@ -1,7 +1,6 @@
-import pycondor, argparse, sys, os.path
+import pycondor
 from glob import glob
 import numpy as np
-import pandas as pd
 
 error = '/scratch/apizzuto/novae/condor/error'
 output = '/scratch/apizzuto/novae/condor/output'
@@ -21,13 +20,13 @@ background_jobs = pycondor.Job(
     submit=submit,
     getenv=True,
     universe='vanilla',
-    verbose=2, 
+    verbose=2,
     request_memory=8000,
     request_cpus=5,
-    extra_lines= ['should_transfer_files = YES', 
+    extra_lines=[
+        'should_transfer_files = YES',
         'when_to_transfer_output = ON_EXIT'],
-    dag=dagman
-	)
+    dag=dagman)
 
 low_mem_signal = pycondor.Job(
     'sensitivity_signal_novae_greco_low_mem',
@@ -38,13 +37,13 @@ low_mem_signal = pycondor.Job(
     submit=submit,
     getenv=True,
     universe='vanilla',
-    verbose=2, 
+    verbose=2,
     request_memory=8000,
     request_cpus=5,
-    extra_lines= ['should_transfer_files = YES', 
+    extra_lines=[
+        'should_transfer_files = YES',
         'when_to_transfer_output = ON_EXIT'],
-    dag=dagman
-	)
+    dag=dagman)
 
 high_mem_signal = pycondor.Job(
     'sensitivity_stacking_novae_greco_high_mem',
@@ -55,52 +54,50 @@ high_mem_signal = pycondor.Job(
     submit=submit,
     getenv=True,
     universe='vanilla',
-    verbose=2, 
+    verbose=2,
     request_memory=12000,
     request_cpus=5,
-    extra_lines= ['should_transfer_files = YES', 
+    extra_lines=[
+        'should_transfer_files = YES',
         'when_to_transfer_output = ON_EXIT'],
-    dag=dagman
-	)
+    dag=dagman)
 
 sample_schemes = [(' --all_nova', 'optical'), ('', 'gamma')]
-#for sample, weighting in sample_schemes:
-#    for deltaT in np.append(np.logspace(-1.5, 1., 6)[:]*86400., np.array([86400.*5.])):
-#        for seed in range(10):
-#            if deltaT > 86400.:
-#                ntrials_bg = 2000
-#            else:
-#                ntrials_bg = 5000
-#            for cut in [0.0, 0.5, 1.0]:
-#                background_jobs.add_arg(
-#                    f"--deltaT={deltaT} --ntrials={ntrials_bg} --minLogE={cut} --seed={seed} --weighting={weighting}{sample}"
-#                )        
+for sample, weighting in sample_schemes:
+    for deltaT in np.append(np.logspace(-1.5, 1., 6)[:]*86400.,
+                            np.array([86400.*5.])):
+        for seed in range(10):
+            if deltaT > 86400.:
+                ntrials_bg = 2000
+            else:
+                ntrials_bg = 5000
+            background_jobs.add_arg(
+                f"--deltaT={deltaT} --ntrials={ntrials_bg} "
+                + f"--seed={seed} --weighting={weighting}{sample}")
 
-allflavor_str = ' --allflavor'
 for sample, weighting in sample_schemes[:1]:
-    for deltaT in np.append(np.logspace(-1.5, 1., 6)[:]*86400., np.array([86400.*5.])):
+    for deltaT in np.append(np.logspace(-1.5, 1., 6)[:]*86400.,
+                            np.array([86400.*5.])):
         if deltaT > 86400.:
             ntrials_sig = 100
         else:
             ntrials_sig = 250
         for gamma in [2.0, 2.5, 3.0]:
-            for cut in [0.0, 0.5, 1.0]:
-                if deltaT > 86400:
-                    high_mem_signal.add_arg(
-                        f'--deltaT={deltaT} --index={gamma}' \
-                        + f' --ntrials_sig={ntrials_sig} --minLogE={cut}' \
-                        + f'{allflavor_str} --weighting={weighting}{sample}'
-                        )
-                else:
-                    low_mem_signal.add_arg(
-                        f'--deltaT={deltaT} --index={gamma}' \
-                        + f' --ntrials_sig={ntrials_sig} --minLogE={cut}' \
-                        + f'{allflavor_str} --weighting={weighting}{sample}'
-                        )
+            if deltaT > 86400:
+                high_mem_signal.add_arg(
+                    f'--deltaT={deltaT} --index={gamma}'
+                    + f' --ntrials_sig={ntrials_sig}'
+                    + f'--weighting={weighting}{sample}'
+                    )
+            else:
+                low_mem_signal.add_arg(
+                    f'--deltaT={deltaT} --index={gamma}'
+                    + f' --ntrials_sig={ntrials_sig}'
+                    + f'--weighting={weighting}{sample}'
+                    )
 
 
-#background_jobs.add_child(low_mem_signal)
-#background_jobs.add_child(high_mem_signal)
+background_jobs.add_child(low_mem_signal)
+background_jobs.add_child(high_mem_signal)
 
 dagman.build_submit()
-
--- scripts/stacking/submit_stacking_signal_jobs.py
+++ scripts/stacking/submit_stacking_signal_jobs.py
@@ -1,7 +1,5 @@
-import pycondor, argparse, sys, os.path
-from glob import glob
+import pycondor
 import numpy as np
-import pandas as pd
 
 error = '/scratch/apizzuto/novae/condor/error'
 output = '/scratch/apizzuto/novae/condor/output'
@@ -21,13 +19,13 @@ low_mem_signal = pycondor.Job(
     submit=submit,
     getenv=True,
     universe='vanilla',
-    verbose=2, 
+    verbose=2,
     request_memory=8000,
     request_cpus=5,
-    extra_lines= ['should_transfer_files = YES', 
+    extra_lines=[
+        'should_transfer_files = YES',
         'when_to_transfer_output = ON_EXIT'],
-    dag=dagman
-	)
+    dag=dagman)
 
 high_mem_signal = pycondor.Job(
     'sensitivity_stacking_novae_greco_high_mem',
@@ -38,36 +36,30 @@ high_mem_signal = pycondor.Job(
     submit=submit,
     getenv=True,
     universe='vanilla',
-    verbose=2, 
+    verbose=2,
     request_memory=12000,
     request_cpus=5,
-    extra_lines= ['should_transfer_files = YES', 
+    extra_lines=[
+        'should_transfer_files = YES',
         'when_to_transfer_output = ON_EXIT'],
-    dag=dagman
-	)
+    dag=dagman)
 
-for allflavor_str in [' --allflavor', '']:
-    for deltaT in np.append(np.logspace(-3., 1., 9)[:]*86400., np.array([86400.*5.])):
-        # for deltaT in [86400.*10.]:
-        if deltaT > 86400.*5.:
-            ntrials_sig = 50
-        elif deltaT > 86400.:
-            ntrials_sig = 100
+for deltaT in np.append(np.logspace(-3., 1., 9)[:]*86400.,
+                        np.array([86400.*5.])):
+    if deltaT > 86400.*5.:
+        ntrials_sig = 50
+    elif deltaT > 86400.:
+        ntrials_sig = 100
+    else:
+        ntrials_sig = 250
+    for gamma in [2.0, 2.5, 3.0]:
+        if deltaT > 86400:
+            high_mem_signal.add_arg(
+                f'--deltaT={deltaT} --index={gamma}'
+                + f' --ntrials_sig={ntrials_sig}')
         else:
-            ntrials_sig = 250
-        for gamma in [2.0, 2.5, 3.0]:
-            # for cut in [0.0, 0.5, 1.0]:
-            if deltaT > 86400:
-                high_mem_signal.add_arg(
-                f'--deltaT={deltaT} --index={gamma}' \
-                    + f' --ntrials_sig={ntrials_sig}' \
-                    + f'{allflavor_str}'
-                    )
-            else:
-                low_mem_signal.add_arg(
-                    f'--deltaT={deltaT} --index={gamma}' \
-                    + f' --ntrials_sig={ntrials_sig}' \
-                    + f'{allflavor_str}'
-                    ) 
+            low_mem_signal.add_arg(
+                f'--deltaT={deltaT} --index={gamma}'
+                + f' --ntrials_sig={ntrials_sig}')
 
 dagman.build_submit()
