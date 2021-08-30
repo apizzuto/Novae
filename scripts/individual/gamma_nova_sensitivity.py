665bbbdf3831e85fc39ec8d79b4b9e0138714f0b
Alex Pizzuto <pizzuto@wisc.edu>
2021-08-30 11:22:43 -0500

    use new dset and pep8 script

--- scripts/individual/gamma_nova_sensitivity.py
+++ scripts/individual/gamma_nova_sensitivity.py
@@ -1,47 +1,47 @@
 #!/usr/bin/env python
 
+import argparse, pickle, sys
+
 import numpy as np
 import pandas as pd
-import astropy as ap
-from astropy.table import Table
-from astropy import units as u
-from astropy.coordinates import SkyCoord
-from astropy.time import Time
-from datetime import datetime
-import argparse
-import matplotlib as mpl
-mpl.use('Agg')
-import histlite as hl
 import csky as cy
-import pickle
-from glob import glob
-import sys
 
-sys.path.append('/home/apizzuto/Nova/scripts/')
-from greco_config import *
-
-parser = argparse.ArgumentParser(description='Sensitivity for individual gamma-ray detected novae')
-parser.add_argument('--deltaT', type=float, default = 1000.,
-                    help='Time window in seconds')
-parser.add_argument('--index', type=float, default=1, help='Spectral Index')
-parser.add_argument('--minLogE', type=float, default=None, help='Cut on the minimum reco energy')
-parser.add_argument('--nova_num', type=int, help="Which nova from the list to use")
-parser.add_argument('--ntrials_bg', type=int, default=10000, help="Number of background trials")
-parser.add_argument('--ntrials_sig', type=int, default=500, help='Number of trials per signal strength')
-parser.add_argument('--full_gamma_time', action='store_true', default=False,
-                        help="Raise if you want to use the full gamma ray time window")
-parser.add_argument('--disc_n_sigma', type=float, default=5., help="Number of sigma for disc. pot")
-parser.add_argument('--disc_CL', type=float, default=0.5, help="Confidence level for disc. pot")
-parser.add_argument('--allflavor', action='store_true', default=False, help="All neutrino flavors in MC")
+parser = argparse.ArgumentParser(
+    description='Sensitivity for individual gamma-ray detected novae')
+parser.add_argument(
+    '--deltaT', type=float, default = 1000.,
+    help='Time window in seconds')
+parser.add_argument(
+    '--index', type=float, default=1, help='Spectral Index')
+parser.add_argument(
+    '--nova_num', type=int, 
+    help="Which nova from the list to use")
+parser.add_argument(
+    '--ntrials_bg', type=int, default=10000, 
+    help="Number of background trials")
+parser.add_argument(
+    '--ntrials_sig', type=int, default=500, 
+    help='Number of trials per signal strength')
+parser.add_argument(
+    '--full_gamma_time', action='store_true', default=False,
+    help="Raise if you want to use the full gamma ray time window")
+parser.add_argument(
+    '--disc_n_sigma', type=float, default=5., 
+    help="Number of sigma for disc. pot")
+parser.add_argument(
+    '--disc_CL', type=float, default=0.5, 
+    help="Confidence level for disc. pot")
 args = parser.parse_args()
 
 delta_t = args.deltaT
 delta_t_days = delta_t / 86400.
 nova_ind = args.nova_num
 
-greco, conf = get_greco_conf(minLogE=args.minLogE, allflavor=args.allflavor)
 ana_dir = cy.utils.ensure_dir('/data/user/apizzuto/csky_cache/greco_ana')
-greco_ana = cy.get_analysis(cy.selections.repo, greco, dir=ana_dir)
+greco_ana = cy.get_analysis(cy.selections.repo,
+    'version-002-p10',
+    cy.selections.GRECOOnlineDataSpecs.GRECO_IC86_2012_2019,
+    dir=ana_dir)
 
 master_df = pd.read_pickle('/home/apizzuto/Nova/master_nova_dataframe.pkl')
 gamma_df = master_df[master_df['gamma']==True]
@@ -54,7 +54,8 @@ try:
     mjd_stop = gamma_df['gamma_stop'][nova_ind].mjd
     name = gamma_df['Name'][nova_ind]
 except:
-    print(f"Only {len(gamma_df)} novae to choose from, index {nova_ind} not available")
+    print(f"Only {len(gamma_df)} novae to choose from" \
+        + f", index {nova_ind} not available")
     sys.exit()
 
 if not args.full_gamma_time:
@@ -68,11 +69,11 @@ if not during_greco:
 delta_t_days = delta_t_days if not args.full_gamma_time else mjd_stop - mjd_start
 
 conf = {'ana': greco_ana,
-       'extended': True,
-       'space': "ps",
-        'time': "transient",
-        'sig': 'transient',
-       }
+    'extended': True,
+    'space': "ps",
+    'time': "transient",
+    'sig': 'transient',
+}
 
 src = cy.utils.Sources(ra=ra, 
                        dec=dec, 
@@ -86,7 +87,11 @@ cy.CONF['mp_cpus'] = 5
 tr = cy.get_trial_runner(conf, ana=greco_ana, src=src)
 bg = cy.dists.Chi2TSD(tr.get_many_fits(args.ntrials_bg))
 
-tr = cy.get_trial_runner(conf, ana=greco_ana, src=src, inj_conf={'flux': cy.hyp.PowerLawFlux(args.index)})
+tr = cy.get_trial_runner(conf, 
+    ana=greco_ana, 
+    src=src, 
+    inj_conf={'flux': cy.hyp.PowerLawFlux(args.index)}
+)
 
 result = {}
 
@@ -119,7 +124,9 @@ discovery['CL'] = args.disc_CL
 ######################## FIT BIAS TRIALS ###############################
 ########################################################################
 n_sigs = np.r_[:51:10]
-trials = [tr.get_many_fits(args.ntrials_sig, n_sig=n_sig, logging=False, seed=n_sig) for n_sig in n_sigs]
+trials = [tr.get_many_fits(
+    args.ntrials_sig, n_sig=n_sig, 
+    logging=False, seed=n_sig) for n_sig in n_sigs]
 for (n_sig, t) in zip(n_sigs, trials):
     t['ntrue'] = np.repeat(n_sig, len(t))
 allt = cy.utils.Arrays.concatenate(trials)
@@ -129,12 +136,13 @@ result['sensitivity'] = sensitivity
 result['discovery'] = discovery
 result['fit'] = allt
 result['settings'] = args
-result['source_info'] = {'ra': ra, 'dec': dec, 'name': name, 'mjd_start': mjd_start, 'mjd_stop': mjd_stop}
+result['source_info'] = {
+    'ra': ra, 'dec': dec, 'name': name, 
+    'mjd_start': mjd_start, 'mjd_stop': mjd_stop}
 
 name = name.replace(' ', '_')
-add_str = 'minLogE_{:.1f}_'.format(args.minLogE) if args.minLogE is not None else ''
 delta_t_str = f"{delta_t:.2e}" if not args.full_gamma_time else "full_gamma_time"
-output_loc = f"/data/user/apizzuto/Nova/csky_trials/nova_{nova_ind}_{name}_delta_t_{delta_t_str}_{add_str}gamma_{args.index}_allflavor_{args.allflavor}_trials.pkl"
+output_loc = f"/data/user/apizzuto/Nova/csky_trials/nova_{nova_ind}_{name}_delta_t_{delta_t_str}_gamma_{args.index}_trials.pkl"
 
 with open(output_loc, 'wb') as f:
     pickle.dump(result, f)
